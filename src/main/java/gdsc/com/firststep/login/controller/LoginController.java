f527e3d6d6cd706cabfbac67eb398dbc3fa7b3d1
Seo, Myoungjin <sih5673@gmail.com>
2023-01-27 21:22:58 +0900

    feat : controller

--- src/main/java/gdsc/com/firststep/login/controller/LoginController.java
+++ src/main/java/gdsc/com/firststep/login/controller/LoginController.java
@@ -1,2 +1,59 @@
-package gdsc.com.firststep.login.controller;public class LoginController {
+package gdsc.com.firststep.login.controller;
+
+import gdsc.com.firststep.login.domain.User;
+import gdsc.com.firststep.login.dto.UserDTO;
+import gdsc.com.firststep.login.dto.UserResponseDTO;
+import gdsc.com.firststep.login.jwt.TokenProvider;
+import gdsc.com.firststep.login.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping
+public class LoginController {
+    //유저 서비스
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    // Bean으로 작성해도 됨.
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+        User user = userService.getByCredentials(
+                userDTO.getUserId(),
+                userDTO.getPassword(),
+                passwordEncoder);
+
+        if(user != null) {
+            // 토큰 생성
+            final String token = tokenProvider.create(user);
+            final UserDTO responseUserDTO = UserDTO.builder()
+                    .userId(user.getUserId())
+                    .token(token)
+                    .age(user.getAge())
+                    .nickname(user.getNickname())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        } else {
+            UserResponseDTO responseDTO = UserResponseDTO.builder()
+                    .error("Login failed.")
+                    .build();
+            return ResponseEntity
+                    .badRequest()
+                    .body(responseDTO);
+        }
+    }
+
 }
--- src/main/java/gdsc/com/firststep/login/controller/SignUpController.java
+++ src/main/java/gdsc/com/firststep/login/controller/SignUpController.java
@@ -1,2 +1,53 @@
-package gdsc.com.firststep.login.controller;public class SignUpController {
+package gdsc.com.firststep.login.controller;
+
+import gdsc.com.firststep.login.domain.User;
+import gdsc.com.firststep.login.dto.UserDTO;
+import gdsc.com.firststep.login.dto.UserResponseDTO;
+import gdsc.com.firststep.login.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping()
+public class SignUpController {
+    @Autowired
+    private UserService userService;
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            // 리퀘스트를 이용해 저장할 유저 만들기
+            User user = User.builder()
+                    .userId(userDTO.getUserId())
+                    .nickname(userDTO.getNickname())
+                    .age(userDTO.getAge())
+                    .password(passwordEncoder.encode(userDTO.getPassword()))
+                    .build();
+            // 서비스를 이용해 리파지토리에 유저 저장
+            User registeredUser = userService.create(user);
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .userId(registeredUser.getUserId())
+                    .Id(registeredUser.getId())
+                    .nickname(registeredUser.getNickname())
+                    .age(registeredUser.getAge())
+                    .build();
+            // 유저 정보는 항상 하나이므로 그냥 리스트로 만들어야하는 ResponseDTO를 사용하지 않고 그냥 UserDTO 리턴.
+            return ResponseEntity.ok(responseUserDTO);
+        } catch (Exception e) {
+            // 예외가 나는 경우 bad 리스폰스 리턴.
+            UserResponseDTO responseDTO = UserResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity
+                    .badRequest()
+                    .body(responseDTO);
+        }
+    }
 }
