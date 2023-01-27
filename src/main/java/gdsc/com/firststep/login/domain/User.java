e2aeacb3f9e540d5f33ee4084867c3da56a25c5b
Seo, Myoungjin <sih5673@gmail.com>
2023-01-27 21:23:42 +0900

    feat : domain

--- src/main/java/gdsc/com/firststep/login/domain/User.java
+++ src/main/java/gdsc/com/firststep/login/domain/User.java
@@ -1,18 +1,12 @@
-package com.example.helpcs.domain.user;
+package gdsc.com.firststep.login.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -27,11 +21,15 @@ public class User {
     private String Id; // 유저에게 고유하게 부여되는 id.
 
     @Column(nullable = false)
-    private String username; // 유저의 이름
+    private String nickname; // 유저의 이름
 
     @Column(nullable = false)
     private String userId; // 유저의 아이디
 
     @Column(nullable = false)
     private String password; // 패스워드
+
+    @Column(nullable = false)
+    private int age; // 나이
+
 }
--- src/main/java/gdsc/com/firststep/login/domain/UserRepository.java
+++ src/main/java/gdsc/com/firststep/login/domain/UserRepository.java
@@ -1,4 +1,4 @@
-package com.example.helpcs.domain.user;
+package gdsc.com.firststep.login.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
